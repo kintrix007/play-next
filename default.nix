a09321a693c15e3f2040df5fb4b8f00244beb357
kintrix007 <boldizsar02@gmail.com>
2023-11-15 11:02:09 +0100

    feat: Make Python also available in nix-shell, not just nix-build

--- default.nix
+++ default.nix
@@ -9,6 +9,12 @@ pkgs.stdenv.mkDerivation rec {
 
   src = ./.;
 
+  # Actually useful to include, because then it is available in nix-shells
+  # as well.
+  # Note: It is not strictly necessary to include python in buildInputs
+  # for the build to succeed, since Nix will infer python being a dependency.
+  buildInputs = [ myPython ];
+
   play-next = pkgs.writeShellScriptBin "play-next" ''
     ${myPython}/bin/python ${src}/main.py
   '';

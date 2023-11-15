d0cbef7cf2f8c884be20f0c179d4497332955b7f
kintrix007 <boldizsar02@gmail.com>
2023-11-15 09:43:27 +0100

    feat: Add Nix build script

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  myPython = (pkgs.python311.withPackages (ps: with ps; [colorama]));
+in
+pkgs.stdenv.mkDerivation rec {
+  pname = "play-next";
+  version = "0.1.1";
+
+  src = ./.;
+
+  installPhase = ''
+    mkdir -p $out/bin
+
+    cat > $out/bin/${pname} <<EOF
+      ${myPython}/bin/python ${src}/main.py
+    EOF
+
+    chmod +x $out/bin/${pname}
+  '';
+}
